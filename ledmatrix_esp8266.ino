10ed46b7e91416e83c0149dc3a636ab28e3b0654
akirasan <akirasan21h@gmail.com>
2016-07-22 00:37:37 +0200

    Add files via upload

---
+++ ledmatrix_esp8266.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include "LedMatrix.h"
+
+
+#define NUMBER_OF_DEVICES 1
+#define CS_PIN 2
+LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
+int x = 0;
+  
+void setup() {
+  ledMatrix.init();
+
+  ledMatrix.setText("akirasan");
+  ledMatrix.setNextText(";)");
+}
+
+void loop() {
+
+  ledMatrix.clear();
+  ledMatrix.scrollTextLeft();
+  ledMatrix.drawText();
+  ledMatrix.commit();
+  delay(50);
+  x=x+1;
+  if (x == 400) {
+     ledMatrix.setNextText("akirasan"); 
+  }
+}
